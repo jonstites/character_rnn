f5b3d34484dcda1122e353b7da0edc365f427f13
Jonathan Stites <mail@jonstites.com>
2017-12-02 22:05:12 -0600

    batched

--- reddbeard.py
+++ reddbeard.py
@@ -7,11 +7,46 @@ import numpy as np
 import tensorflow as tf
 
 
+ascii_pad_character = 256
+
+
 def get_sequences(records):
     with bz2.BZ2File(records, "rb") as dataset:
-        for line in dataset:
-            data = json.loads(line)
+        lines = dataset.readlines()
+        for line in lines:
+            data = json.loads(line.decode("UTF-8"))
             yield data["body"]
+
+def get_batch(records, batch_size=32, max_length=300):
+    batch = []
+    lengths = []
+    for seq in get_sequences(records):
+        if len(seq) >= max_length:
+            continue
+        if not is_ascii(seq):
+            continue
+        length = len(seq)
+        processed = ascii_to_integers(seq)
+        padded = pad_ascii(processed, max_length)
+        batch.append(padded)
+        lengths.append(length)
+        if len(batch) >= batch_size:
+            yield batch, lengths
+            batch = []
+            lengths = []
+    if len(batch) > 0:
+        yield batch, lengths
+
+def is_ascii(seq):
+    return len(seq) == len(seq.encode())
+
+def ascii_to_integers(seq):
+    return [ord(letter) for letter in seq]
+
+def pad_ascii(seq, max_length):
+    pad_num = max_length - len(seq) 
+    return seq + [ascii_pad_character]*pad_num
+        
 
 def main(train_records):
 
@@ -19,7 +54,7 @@ def main(train_records):
     tf.reset_default_graph()
 
     # Create input data
-    batch_size=1
+    batch_size=16
 
     alpha_size = 256
 
@@ -32,17 +67,20 @@ def main(train_records):
     labels = tf.one_hot(pre_labels, alpha_size)
 
     
-    cell = tf.nn.rnn_cell.LSTMCell(num_units=alpha_size)
+    cell1 = tf.nn.rnn_cell.GRUCell(num_units=alpha_size)
+    cell2 = tf.nn.rnn_cell.GRUCell(num_units=alpha_size)
+    cell = tf.nn.rnn_cell.MultiRNNCell([cell1, cell2])
 
-    outputs, states = tf.nn.dynamic_rnn(
+    pre_outputs, states = tf.nn.dynamic_rnn(
         cell=cell,
         dtype=tf.float32,
         sequence_length=X_lengths,
         inputs=X)
-    print(outputs)
+    outputs = tf.contrib.layers.batch_norm(pre_outputs)
     outputs_flat = tf.reshape(outputs, [-1, alpha_size])
     Ylogits = tf.contrib.layers.linear(outputs_flat, alpha_size)     # [ BATCHSIZE x SEQLEN, ALPHASIZE ]
     logits = Ylogits
+    soft_logits = tf.nn.softmax(logits)
     Yflat_ = tf.reshape(labels, [-1, alpha_size])     # [ BATCHSIZE x SEQLEN, ALPHASIZE ]
     print("yflat", Yflat_)
     print("logits", logits)
@@ -58,23 +96,42 @@ def main(train_records):
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
-    for epoch in range(10000):
-        for comment in get_sequences(train_records):
-            characters = [ord(character) for character in comment]
-            seq_length = len(characters) 
+    
+    for epoch in range(100):
+        #for comment in get_sequences(train_records):
+        avg_loss = []
+        for num, batch in enumerate(get_batch(train_records, batch_size=batch_size)):
+            sequences, lengths = batch
+            input_sequences = [seq[:-1] for seq in sequences]
+            output_sequences = [seq[1:] for seq in sequences]
 
-
-            x = np.reshape(characters, (batch_size, seq_length))
-            x2 = x[:,:-1]
-            y = x[:,1:]
-            the_loss, _, the_logits, the_labels, the_outputs = sess.run([loss, optimizer, logits, labels, Yflat_], feed_dict={pre_X:x2, X_lengths:[seq_length-1], pre_labels:y})
-
-            if epoch % 10 == 0:
+            the_loss, _, the_logits, the_labels, the_outputs = sess.run([loss, optimizer, logits, labels, Yflat_], feed_dict={pre_X:input_sequences, X_lengths:lengths, pre_labels:output_sequences})
+            real_logits = np.reshape(the_logits, (len(sequences), 299, alpha_size))
+            avg_loss.append(the_loss)
+            
+            if num % 100 == 0 and num != 0:
                 print("epoch:", epoch)
-                print("loss:", the_loss)
-                print("".join([chr(char) for char in characters[1:]]))
-                print("".join([chr(np.argmax(the_logits[i])) for i in range(seq_length-1)]), flush=True)
+                print("batch:", num)
+                print("loss:", np.mean(avg_loss))
+                avg_loss = []
 
+                
+                for i in range(len(sequences)):
+                    sequence = sequences[i]
+                    length = lengths[i]
+                    print("".join([chr(sequence[j]) for j in range(length)]))
+                    print("".join([chr(np.argmax(real_logits[i][j])) for j in range(length)]), flush=True)
 
+                """
+                seq = ascii_to_integers(np.random.choice(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")))
+                seq = pad_ascii(seq, 300)
+                for i in range(1, 299):
+                    probs = sess.run(soft_logits, feed_dict={pre_X:[seq], X_lengths:[i]})
+                    new_char = np.random.choice(alpha_size, p=probs[i-1])
+                    seq[i] = new_char
+                print("A sample:")
+                print("".join([chr(s) for s in seq]), flush=True)
+                """
+                    
 if __name__ == "__main__":
     argh.dispatch_command(main)
