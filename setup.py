18cd5c0bf38bb0a5c547a5859de8abdc1357839f
Jonathan Stites <mail@jonstites.com>
2017-12-27 14:29:54 -0600

    package up the code

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='character_rnn',
+      version='0.1',
+      description='A character-by-character RNN.',
+      url='http://github.com/jonstites/character_rnn',
+      author='Jonathan Stites',
+      author_email='mail@jonstites.com',
+      license='MIT',
+      packages=['character_rnn'],
+      install_requires=[
+          'argh',
+          'numpy',
+          'tensorflow'
+          ],
+      zip_safe=False)
