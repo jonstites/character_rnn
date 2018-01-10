40bb43a483f2560961641c2752d0a09ff735a265
Jonathan Stites <mail@jonstites.com>
2018-01-10 17:31:18 -0600

    added tests, tried character embeddings

---
+++ character_rnn/utils.py
@@ -0,0 +1,139 @@
+from collections import Counter
+import math
+import numpy as np
+
+
+class Text:
+
+    def load_file(text_file):
+        with open(text_file) as handle:
+            return handle.read()
+    
+    def load_files(text_files):
+        texts = []
+        for text_file in text_files:
+            text = Text.load_file(text_file)
+            texts.append(text)
+        return texts
+
+    def to_ids(texts, ids):
+        as_ids = []
+        for text in texts:
+            as_id = [ids[char] for char in text]
+            as_ids.append(as_id)
+        return as_ids
+    
+    def _texts_to_ids(texts):
+        counts = Text._multi_count_characters(texts)
+        sorted_characters = Text._sort_keys_by_freq_and_lex(counts)
+        character_ids = {}
+        for index, character in enumerate(sorted_characters):
+            character_ids[character] = index
+        return character_ids
+
+    def _sort_keys_by_freq_and_lex(counts):
+        return sorted(counts.keys(), key=lambda x: (-counts[x], x))
+    
+    def _count_characters(text):
+        counts = Counter(text)
+        counts[""] = 0        
+        return counts
+
+    def _multi_count_characters(texts):
+        counts = Counter()
+        for text in texts:
+            counts.update(text)
+        counts[""] = 0
+        return counts
+
+    def chunk(text, sequence_length):
+        chunks = []
+        for start in range(0, len(text), sequence_length):
+            chunk = text[start: start + sequence_length]
+            if len(chunk) < sequence_length:
+                break
+            chunks.append(chunk)
+        return chunks
+        
+    
+class Dataset:
+
+    def __init__(self):
+        self.files = []
+        self.sources = []
+        self.texts = []
+
+    def add_file(self, filename, source=None):
+        self.files.append(filename)
+        self.sources.append(source)
+
+    def add_files(self, filenames, sources=None):
+        if sources is None:
+            sources = [None for _ in len(filenames)]
+        
+        assert len(filenames) == len(sources)
+
+        for filename, source in zip(filenames, sources):
+            self.add_file(filename, source)        
+
+    def preprocess(self):
+        self.load_files()
+        self.create_vocabulary()
+        self.train_val_test_split()
+            
+    def load_files(self):
+        texts = Text.load_files(self.files)
+        self.texts = texts
+        
+    def create_vocabulary(self):
+        self.vocabulary = Text._texts_to_ids(self.texts)
+        self.texts = Text.to_ids(self.texts, self.vocabulary)
+    
+    def train_val_test_split(self, val_fraction=0.1, test_fraction=0.1, sequence_length=100):
+        batches = []
+        sources = []
+        truncated_sequence = 0
+        for source, text in zip(self.sources, self.texts):
+            chunks = Text.chunk(text, sequence_length)
+            batches += chunks
+            truncated_sequence += len(text) % sequence_length
+            sources += [source] * len(chunks)
+            
+        print("Truncated ", truncated_sequence, " characters.")
+        np.random.seed(0)
+        np.random.shuffle(batches)
+        np.random.seed(0)
+        np.random.shuffle(sources)
+
+        num_val = math.ceil(len(batches) * val_fraction)
+        num_test = math.ceil(len(batches) * test_fraction)
+
+
+        # convert to np here?
+        self.validation_batches = batches[:num_val]
+        self.validation_sources = sources[:num_val]
+        self.test_batches = batches[num_val:num_val+num_test]
+        self.test_sources = sources[num_val:num_val+num_test]
+        self.train_batches = batches[num_val+num_test:]
+        self.train_sources = sources[num_val+num_test:]        
+
+def epoch_batches_sequence(batches, sources, epoch, batch_size=128):
+
+    seed = epoch
+
+    if len(batches) < batch_size:
+        print("not enough batches for batch size")
+        return
+    
+    while True:
+        np.random.seed(seed)
+        np.random.shuffle(batches)
+        np.random.seed(seed)
+        np.random.shuffle(sources)
+
+        for start in range(0, len(batches), batch_size):
+            batch = batches[start: start + batch_size]
+            if len(batch) != batch_size:
+                break
+            source = sources[start: start + batch_size]
+            yield batch, source
---
+++ test_utils.py
@@ -0,0 +1,130 @@
+from character_rnn import utils
+import io
+import os
+import pytest
+
+@pytest.fixture()
+def small_text():
+    s = "a\nba"
+    return s
+
+@pytest.fixture()
+def small_text_2():
+    s = "zAa"
+    return s
+
+@pytest.fixture()
+def small_file(tmpdir):
+    temp_file = os.path.join(tmpdir, "small_file.txt")
+    with open(temp_file, 'w') as handle:
+        handle.write(small_text())
+    return temp_file
+
+@pytest.fixture()
+def small_file_2(tmpdir):
+    temp_file = os.path.join(tmpdir, "small_file_2.txt")
+    with open(temp_file, 'w') as handle:
+        handle.write(small_text_2())
+    return temp_file
+
+@pytest.fixture()
+def small_texts():
+    texts = [small_text(), small_text_2()]
+    return texts
+
+@pytest.fixture()
+def small_dataset(tmpdir):
+    text_file = small_file(tmpdir)
+    text_file_2 = small_file_2(tmpdir)
+    dataset = utils.Dataset()
+
+    dataset.add_file(text_file)
+    dataset.add_file(text_file_2, source="atest")
+    return dataset
+    
+class TestText:
+
+    def test_load_text_file(self, tmpdir):
+        target = small_file(tmpdir)
+        text = utils.Text.load_file(target)
+        assert text == small_text()
+
+    def test_load_text_files(self, tmpdir):
+        target = small_file(tmpdir)
+        targets = [target, target]
+        texts = utils.Text.load_files(targets)
+        assert texts == [small_text(), small_text()]
+
+    def test_count_characters(self):
+        text = small_text()
+        counts = {"": 0, "a": 2, "b": 1, "\n": 1}
+        assert utils.Text._count_characters(text) == counts
+        
+    def test_multi_count_characters(self):
+        texts = small_texts()
+        counts = {"a": 3, "b": 1, "\n": 1, "z": 1, "A": 1, "": 0}
+        assert utils.Text._multi_count_characters(texts) == counts
+
+    def test_sort_keys_by_freq_and_lex(self):
+        texts = small_texts()
+        counts = utils.Text._multi_count_characters(texts)
+        sorted_keys = ["a", "\n", "A", "b", "z", ""]
+        assert utils.Text._sort_keys_by_freq_and_lex(counts) == sorted_keys
+        
+    def test_texts_to_ids(self):
+        texts = small_texts()
+        ids = {"a": 0, "\n": 1, "A": 2, "b": 3, "z": 4, "": 5}
+        assert utils.Text._texts_to_ids(texts) == ids
+
+    def test_to_ids(self):
+        texts = small_texts()
+        as_ids = [[0, 1, 3, 0], [ 4, 2, 0]]
+        assert utils.Text.to_ids(texts, utils.Text._texts_to_ids(texts)) == as_ids
+
+    def test_chunk_text(self):
+        text = small_text()
+        chunks = utils.Text.chunk(text, 3)
+        assert chunks == ["a\nb"]
+
+class TestDataset:
+
+    def test_init(self):
+        dataset = utils.Dataset()
+        assert dataset.files == []
+        assert dataset.sources == []
+        
+    def test_add_file(self, tmpdir):
+        dataset = small_dataset(tmpdir)
+        
+        assert dataset.files[0] == small_file(tmpdir)
+        assert dataset.files[1] == small_file_2(tmpdir)
+        assert dataset.sources[0] == None
+        assert dataset.sources[1] == "atest"
+
+    def test_add_files(self, tmpdir):
+        dataset = small_dataset(tmpdir)
+
+        text_file = small_file(tmpdir)
+        text_file_2 = small_file_2(tmpdir)
+        dataset = utils.Dataset()
+        dataset.add_files([text_file, text_file_2], [None, "atest"])
+        
+        assert dataset.files[0] == small_file(tmpdir)
+        assert dataset.files[1] == small_file_2(tmpdir)
+        assert dataset.sources[0] == None
+        assert dataset.sources[1] == "atest"
+        
+    def test_load_files(self, tmpdir):
+        dataset = small_dataset(tmpdir)
+        dataset.load_files()
+        
+        assert dataset.texts == small_texts()
+        
+    def test_train_val_test_split(self, tmpdir):
+        dataset = small_dataset(tmpdir)
+        dataset.load_files()
+        dataset.train_val_test_split(test_fraction=0.0, sequence_length=1)
+
+        assert len(dataset.validation_batches) == 1
+        assert len(dataset.train_batches) == 6
+        assert len(dataset.test_batches) == 0
